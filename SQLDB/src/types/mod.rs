ad51d1e5ecc9b7344c616a7ef06c9e93858d8ede
Catherine Zhao <zhiruoz2@illinois.edu>
2024-12-26 08:43:51 +0800

    define all types

---
+++ SQLDB/src/types/mod.rs
@@ -0,0 +1,44 @@
+use serde::{Deserialize, Serialize};
+
+use super::parser::ast::{Consts, Expression};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum DataType {
+    Boolean,
+    Integer,
+    Float,
+    String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum Value {
+    Null,
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+}
+
+impl Value {
+    pub fn from_expression(expr: Expression) -> Self {
+        match expr {
+            Expression::Consts(Consts::Null) => Self::Null,
+            Expression::Consts(Consts::Boolean(b)) => Self::Boolean(b),
+            Expression::Consts(Consts::Integer(i)) => Self::Integer(i),
+            Expression::Consts(Consts::Float(f)) => Self::Float(f),
+            Expression::Consts(Consts::String(s)) => Self::String(s),
+        }
+    }
+
+    pub fn datatype(&self) -> Option<DataType> {
+        match self {
+            Self::Null => None,
+            Self::Boolean(_) => Some(DataType::Boolean),
+            Self::Integer(_) => Some(DataType::Integer),
+            Self::Float(_) => Some(DataType::Float),
+            Self::String(_) => Some(DataType::String),
+        }
+    }
+}
+
+pub type Row = Vec<Value>;
