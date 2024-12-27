c4f0e3394460dd01887d5e6fc3bdaaec0d3f81f3
Catherine Zhao <zhiruoz2@illinois.edu>
2024-12-28 07:27:05 +0800

    build engine query

---
+++ SQLDB/src/sql/executor/query.rs
@@ -0,0 +1,24 @@
+use crate::{error::Result, sql::engine::Transaction};
+
+use super::{Executor, ResultSet};
+
+pub struct Scan {
+    table_name: String,
+}
+
+impl Scan {
+    pub fn new(table_name: String) -> Box<Self> {
+        Box::new(Self { table_name })
+    }
+}
+
+impl<T: Transaction> Executor<T> for Scan {
+    fn execute(self: Box<Self>, txn: &mut T) -> Result<ResultSet> {
+        let table = txn.must_get_table(self.table_name.clone())?;
+        let rows = txn.scan_table(self.table_name.clone())?;
+        Ok(ResultSet::Scan {
+            columns: table.columns.into_iter().map(|c| c.name.clone()).collect(),
+            rows,
+        })
+    }
+}
