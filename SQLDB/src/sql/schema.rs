5ac5f93f1563716cbf9aa972fa5901fa078e8850
Catherine Zhao <zhiruoz2@illinois.edu>
2024-12-28 07:32:27 +0800

    SQL schema

---
+++ SQLDB/src/sql/schema.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+use super::types::{DataType, Value};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Table {
+    pub name: String,
+    pub columns: Vec<Column>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Column {
+    pub name: String,
+    pub datatype: DataType,
+    pub nullable: bool,
+    pub default: Option<Value>,
+}
