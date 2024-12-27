db2ac390a8de9041f0e1340fe50c435d486fd886
Catherine Zhao <zhiruoz2@illinois.edu>
2024-12-28 07:30:42 +0800

    module for executor

---
+++ SQLDB/src/sql/executor/mod.rs
@@ -0,0 +1,43 @@
+use super::{engine::Transaction, plan::Node, types::Row};
+use crate::error::Result;
+use mutation::Insert;
+use query::Scan;
+use schema::CreateTable;
+
+mod mutation;
+mod query;
+mod schema;
+
+// Define executor
+pub trait Executor<T: Transaction> {
+    fn execute(self: Box<Self>, txn: &mut T) -> Result<ResultSet>;
+}
+
+impl<T: Transaction> dyn Executor<T> {
+    pub fn build(node: Node) -> Box<dyn Executor<T>> {
+        match node {
+            Node::CreateTable { schema } => CreateTable::new(schema),
+            Node::Insert {
+                table_name,
+                columns,
+                values,
+            } => Insert::new(table_name, columns, values),
+            Node::Scan { table_name } => Scan::new(table_name),
+        }
+    }
+}
+
+// Result set of executions
+#[derive(Debug)]
+pub enum ResultSet {
+    CreateTable {
+        table_name: String,
+    },
+    Insert {
+        count: usize,
+    },
+    Scan {
+        columns: Vec<String>,
+        rows: Vec<Row>,
+    },
+}
