e6aed291cf86a70ae9304de9fc44b2f5dcf6e237
Catherine Zhao <zhiruoz2@illinois.edu>
2024-12-28 20:22:19 +0800

    introduce custom error type for DB operations

---
+++ SQLDB/target/debug/deps/SQLDB-23d5f12e49059b09.d
@@ -0,0 +1,25 @@
+/Users/cathzzr2/Desktop/SQL-DB/SQLDB/target/debug/deps/libSQLDB-23d5f12e49059b09.rmeta: src/lib.rs src/error.rs src/sql/mod.rs src/sql/engine/mod.rs src/sql/engine/kv.rs src/sql/executor/mod.rs src/sql/executor/mutation.rs src/sql/executor/query.rs src/sql/executor/schema.rs src/sql/parser/mod.rs src/sql/parser/lexer.rs src/sql/plan/mod.rs src/sql/plan/planner.rs src/sql/schema.rs src/sql/types/mod.rs src/storage/mod.rs src/storage/disk.rs src/storage/engine.rs src/storage/keycode.rs src/storage/memory.rs src/storage/mvcc.rs
+
+/Users/cathzzr2/Desktop/SQL-DB/SQLDB/target/debug/deps/SQLDB-23d5f12e49059b09.d: src/lib.rs src/error.rs src/sql/mod.rs src/sql/engine/mod.rs src/sql/engine/kv.rs src/sql/executor/mod.rs src/sql/executor/mutation.rs src/sql/executor/query.rs src/sql/executor/schema.rs src/sql/parser/mod.rs src/sql/parser/lexer.rs src/sql/plan/mod.rs src/sql/plan/planner.rs src/sql/schema.rs src/sql/types/mod.rs src/storage/mod.rs src/storage/disk.rs src/storage/engine.rs src/storage/keycode.rs src/storage/memory.rs src/storage/mvcc.rs
+
+src/lib.rs:
+src/error.rs:
+src/sql/mod.rs:
+src/sql/engine/mod.rs:
+src/sql/engine/kv.rs:
+src/sql/executor/mod.rs:
+src/sql/executor/mutation.rs:
+src/sql/executor/query.rs:
+src/sql/executor/schema.rs:
+src/sql/parser/mod.rs:
+src/sql/parser/lexer.rs:
+src/sql/plan/mod.rs:
+src/sql/plan/planner.rs:
+src/sql/schema.rs:
+src/sql/types/mod.rs:
+src/storage/mod.rs:
+src/storage/disk.rs:
+src/storage/engine.rs:
+src/storage/keycode.rs:
+src/storage/memory.rs:
+src/storage/mvcc.rs:
